656c788f31f6e23fa4860c68653f3914cec0c7af
Steven Bister <steven.bister@thrivelearning.com>
2026-02-19 16:51:51 +0000

    fix api url
    looks like the domain has been updated

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,4 +1,4 @@
-export const HEVY_WORKOUT = 'https://api.hevy.com/v1/workouts' as const;
+export const HEVY_WORKOUT = 'https://api.hevyapp.com/v1/workouts' as const;
 
 export const FITBIT_TOKEN = 'https://api.fitbit.com/oauth2/token' as const;
 export const FITBIT_CREATE_ACTIVITY =
